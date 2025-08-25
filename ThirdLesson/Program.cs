3d3b5d3f94d9d8d1047a2a08c9b513fa0c8636a0
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-08-25 03:08:45 -0300

    First Program, Salary Calculation - Basic.

---
+++ ThirdLesson/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculateSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de horas trabajadas: ");
+            int horasTrabajadas = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el costo por hora: $");
+            double costoPorHora = double.Parse(Console.ReadLine());
+
+            double sueldo = (double)horasTrabajadas * costoPorHora;
+            Console.WriteLine("El sueldo a percibir el empleado ingresado es de: $" + sueldo.ToString());
+            Console.ReadKey(); // This line makes that the console don't disapear, waits a key from the user.
+        }
+    }
+}
